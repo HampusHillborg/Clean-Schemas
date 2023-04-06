e65f36cef3dc283d4315b9a34f32fc3594b62173
Imaznaseem <imaznaseem@gmail.com>
2023-04-06 03:08:13 +0200

    Jag har lagt till en JButton med namnet "submitButton", som när den klickas läser inmatningen från formulärfälten, skapar ett profilobjekt och visar användarens profil med en ny instans.

--- src/Boundary/ProfileFormGUI.java
+++ src/Boundary/ProfileFormGUI.java
@@ -1,115 +1,97 @@
 package src.Boundary;
 
-
 import src.Entity.Profile;
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    public class ProfileFormGUI extends JFrame {
-        // Text fields for user input
-        private JTextField heightField;
-        private JTextField weightField;
-        private JTextField ageField;
-        private JComboBox<String> sexBox;
-        private JComboBox<String> goalBox;
-        private JComboBox<String> carbBox;
-        private JTextField mealsField;
+public class ProfileFormGUI extends JFrame {
+    private JTextField heightField;
+    private JTextField weightField;
+    private JTextField ageField;
+    private JComboBox<String> sexField;
+    private JComboBox<String> goalField;
+    private JComboBox<String> carbField;
+    private JComboBox<String> mealsField;
 
-        // Labels for input fields
-        private JLabel heightLabel;
-        private JLabel weightLabel;
-        private JLabel ageLabel;
-        private JLabel sexLabel;
-        private JLabel goalLabel;
-        private JLabel carbLabel;
-        private JLabel mealsLabel;
+    public ProfileFormGUI() {
+        super("User Profile Form");
 
-        // Button for submitting input
-        private JButton submitButton;
+        // Create components
+        JLabel titleLabel = new JLabel("Enter Your Profile Information");
+        JLabel heightLabel = new JLabel("Height (cm):");
+        JLabel weightLabel = new JLabel("Weight (kg):");
+        JLabel ageLabel = new JLabel("Age:");
+        JLabel sexLabel = new JLabel("Sex:");
+        JLabel goalLabel = new JLabel("Goal:");
+        JLabel carbLabel = new JLabel("Carbohydrate Intake:");
+        JLabel mealsLabel = new JLabel("Number of Meals:");
 
-        public ProfileFormGUI() {
-            super("User Profile Form");
+        heightField = new JTextField();
+        weightField = new JTextField();
+        ageField = new JTextField();
+        sexField = new JComboBox<>(new String[]{"Male", "Female"});
+        goalField = new JComboBox<>(new String[]{"Weight Loss", "Maintenance", "Weight Gain"});
+        carbField = new JComboBox<>(new String[]{"Low", "Medium", "High"});
+        mealsField = new JComboBox<>(new String[]{"1", "2", "3", "4", "5"});
 
-            // Initialize text fields
-            heightField = new JTextField(10);
-            weightField = new JTextField(10);
-            ageField = new JTextField(10);
-            sexBox = new JComboBox<>(new String[]{"Male", "Female"});
-            goalBox = new JComboBox<>(new String[]{"Weight Loss", "Weight Gain"});
-            carbBox = new JComboBox<>(new String[]{"Low", "Medium", "High"});
-            mealsField = new JTextField(10);
+        JButton submitButton = new JButton("Submit");
 
-            // Initialize labels
-            heightLabel = new JLabel("Height (cm): ");
-            weightLabel = new JLabel("Weight (kg): ");
-            ageLabel = new JLabel("Age: ");
-            sexLabel = new JLabel("Sex: ");
-            goalLabel = new JLabel("Goal: ");
-            carbLabel = new JLabel("Carbohydrate Intake: ");
-            mealsLabel = new JLabel("Number of Meals: ");
+        // Create layout and add components
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(8, 2));
+        panel.add(heightLabel);
+        panel.add(heightField);
+        panel.add(weightLabel);
+        panel.add(weightField);
+        panel.add(ageLabel);
+        panel.add(ageField);
+        panel.add(sexLabel);
+        panel.add(sexField);
+        panel.add(goalLabel);
+        panel.add(goalField);
+        panel.add(carbLabel);
+        panel.add(carbField);
+        panel.add(mealsLabel);
+        panel.add(mealsField);
+        panel.add(submitButton);
 
-            // Initialize submit button
-            submitButton = new JButton("Submit");
-            submitButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    // Get user input from text fields and combo boxes
-                    int height = Integer.parseInt(heightField.getText());
-                    int weight = Integer.parseInt(weightField.getText());
-                    int age = Integer.parseInt(ageField.getText());
-                    String sex = (String) sexBox.getSelectedItem();
-                    String goal = (String) goalBox.getSelectedItem();
-                    String carbAmount = (String) carbBox.getSelectedItem();
-                    int mealsPerDay = Integer.parseInt(mealsField.getText());
+        // Set window properties
+        setLayout(new BorderLayout());
+        add(titleLabel, BorderLayout.NORTH);
+        add(panel, BorderLayout.CENTER);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
 
+        // Add submit button listener
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Get user input from fields
+                int height = Integer.parseInt(heightField.getText());
+                int weight = Integer.parseInt(weightField.getText());
+                int age = Integer.parseInt(ageField.getText());
+                String sex = (String) sexField.getSelectedItem();
+                String goal = (String) goalField.getSelectedItem();
+                String carbAmount = (String) carbField.getSelectedItem();
+                int mealsPerDay = Integer.parseInt((String) mealsField.getSelectedItem());
 
+                // Create Profile object with user input
+                Profile userProfile = new Profile(height, weight, age, sex, goal, carbAmount, mealsPerDay);
 
-                    // Create new Profile object and set user input
-                    Profile userProfile = new Profile(height, weight, age, sex, goal, carbAmount, mealsPerDay /*favoriteFoods, dislikedFoods*/);
-                    userProfile.setHeight(height);
-                    userProfile.setWeight(weight);
-                    userProfile.setAge(age);
-                    userProfile.setSex(sex);
-                    userProfile.setGoal(goal);
-                    userProfile.setCarbAmount(carbAmount);
-                    userProfile.setMealsPerDay(mealsPerDay);
-
-                    // Save user input to database or perform other actions as needed
-
-                    // Show message to confirm submission
-                    JOptionPane.showMessageDialog(null, "Profile submitted!");
-                }
-            });
-
-            // Create layout and add components
-            setLayout(new GridLayout(8, 2, 10, 10));
-            add(heightLabel);
-            add(heightField);
-            add(weightLabel);
-            add(weightField);
-            add(ageLabel);
-            add(ageField);
-            add(sexLabel);
-            add(sexBox);
-            add(goalLabel);
-            add(goalBox);
-            add(carbLabel);
-            add(carbBox);
-            add(mealsLabel);
-            add(mealsField);
-            add(new JLabel(""));
-            add(submitButton);
-
-            // Set window properties
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setSize(400, 300);
-            setLocationRelativeTo(null);
-            setVisible(true);
-        }
-
-        public static void main(String[] args) {
-            ProfileFormGUI gui = new ProfileFormGUI();
-        }
+                // Create and show ProfileDisplayGUI with user profile
+                ProfileDisplayGUI displayGUI = new ProfileDisplayGUI(userProfile);
+                dispose();
+            }
+        });
     }
+
+    public static void main(String[] args) {
+        ProfileFormGUI formGUI = new ProfileFormGUI();
+    }
+}
+
