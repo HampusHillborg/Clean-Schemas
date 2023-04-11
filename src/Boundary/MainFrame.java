95279473c00b44c80151e5cab855925d9d890c42
Imaznaseem <imaznaseem@gmail.com>
2023-04-11 18:36:46 +0200

    Create a MainFrame Class where i Use instances of LandingPage class and LoginViewerGUI

--- src/Boundary/MainFrame.java
+++ src/Boundary/MainFrame.java
@@ -23,7 +23,28 @@ public class MainFrame extends JFrame {
     }
 
 
+    public void showLandingPage() {
+        // Create the LandingPage and remove the LoginViewerGUI
+        this.landingPage = new LandingPage();
+        getContentPane().remove(this.loginViewerGUI);
+
+        // Add the LandingPage to the content pane of the JFrame
+        getContentPane().add(this.landingPage, BorderLayout.CENTER);
+
+        // Refresh the JFrame
+        revalidate();
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        // Create a new MainFrame object
+        MainFrame mainFrame = new MainFrame();
+
+        // Set the LoginViewerGUI's listener to the MainFrame's showLandingPage method
+        //mainFrame.loginViewerGUI.setLoginListener(mainFrame::showLandingPage);
+    }
 }
+
 
 
 
