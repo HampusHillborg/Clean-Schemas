69d31a6551a09efa94d4b21f3177d0dec5ae0b3b
HampusHillborg <hampushillborg@gmail.com>
2023-04-13 13:47:46 +0200

    moved updateProfile to LoginController so that when trying to update, you dont have to log in again

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -17,11 +17,18 @@ public class LoginController {
     private String username;
     private Profile loggedInUser;
     private UserDatabaseOutput databaseOutput;
+    private MacronutrientControl macroControl;
+    private final double LOW_CARBS = 0.2;
+    private final double MEDIUM_CARBS = 0.35;
+    private final double HIGH_CARBS = 0.5;
+    private int tdee;
+    private int bmr;
 
     public LoginController() {
         // Initialize database connection
         userDatabase = new UserDatabase();
         databaseOutput = new UserDatabaseOutput();
+        this.macroControl = new MacronutrientControl();
 
     }
 
@@ -52,6 +59,41 @@ public class LoginController {
         this.loggedInUser = new Profile(username, password);
         loggedInUser.addToProfile(height, weight, age, sex, goal, activityValue, carbAmount,mealsPerDay);
         return loggedInUser;
+    }
+
+    public void updateProfile(Profile userProfile){
+        // Save user input to database
+
+        try {
+            int userId = userDatabase.getUserId(userProfile.getEmail());
+            userDatabase.addSex(userId, userProfile.getSex());
+            userDatabase.addWeight(userId, userProfile.getWeight());
+            userDatabase.addHeight(userId, userProfile.getHeight());
+            userDatabase.addActivityValue(userId, userProfile.getActivityValue());
+            userDatabase.addAmountOfCarbs(userId, userProfile.getCarbAmount());
+            userDatabase.addGoal(userId, userProfile.getGoal());
+            userDatabase.addMealsPerDay(userId, userProfile.getMealsPerDay());
+            userDatabase.addAge(userId, userProfile.getAge());
+            // Calculate BMR and TDEE
+            bmr = macroControl.calculateBmr(userProfile.getWeight(), userProfile.getHeight(), userProfile.getAge(), userProfile.getSex());
+            userProfile.setBmr(bmr);
+            macroControl.setActivityLevel(userProfile.getActivityValue()); // set activityLevel
+            tdee = macroControl.calculateTdee(bmr, userProfile.getActivityValue());
+            tdee = macroControl.adjustTdeeForGoal(tdee, userProfile.getGoal());
+            userProfile.setTdee(tdee);
+
+            // Update userProfile object with the new BMR and TDEE values
+            userProfile.setBmr(bmr);
+            userProfile.setTdee(tdee);
+
+            // Save BMR and TDEE to database
+            userId = userDatabase.getUserId(userProfile.getEmail());
+            userDatabase.addBmr(userId, bmr);
+            userDatabase.addTdee(userId, tdee);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
