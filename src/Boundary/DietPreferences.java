134500c6b538e599f35c46bd3bebb60268a01027
AdrPou <adrian.pourlotfi@gmail.com>
2023-04-18 19:47:11 +0200

    Created DietPreferences
    Trying to figure out panel sizes

---
+++ src/Boundary/DietPreferences.java
@@ -0,0 +1,112 @@
+package src.Boundary;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DietPreferences {
+    private JFrame frame;
+    private JPanel mainPanel;
+    private JCheckBox[] dietCheckboxes;
+    private JTextField searchField;
+    private JButton likeButton;
+    private JButton dislikeButton;
+    private JList<String> likeList;
+    private JList<String> dislikeList;
+    private JButton submitButton;
+    private DefaultListModel<String> likeListModel;
+    private DefaultListModel<String> dislikeListModel;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                DietPreferences window = new DietPreferences();
+                window.frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public DietPreferences() {
+        initialize();
+    }
+
+    private void initialize() {
+        frame = new JFrame("Diet Preferences");
+        frame.setBounds(100, 100, 800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new BorderLayout());
+    
+        mainPanel = new JPanel();
+        frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+    
+        JPanel checkboxPanel = new JPanel();
+        checkboxPanel.setLayout(new BoxLayout(checkboxPanel, BoxLayout.Y_AXIS));
+        String[] diets = {"Vegetarian", "Gluten-free", "Vegan", "Dairy-free", "Carnivore", "Keto", "Islamic", "Pescatarian", "Keto", "Paleo"};
+        dietCheckboxes = new JCheckBox[diets.length];
+        for (int i = 0; i < diets.length; i++) {
+            dietCheckboxes[i] = new JCheckBox(diets[i]);
+            checkboxPanel.add(dietCheckboxes[i]);
+        }
+        mainPanel.add(checkboxPanel);
+    
+        JPanel listPanel = new JPanel();
+        listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
+        searchField = new JTextField(20);
+        listPanel.add(searchField);
+    
+        likeButton = new JButton("Like");
+        dislikeButton = new JButton("Dislike");
+        listPanel.add(likeButton);
+        listPanel.add(dislikeButton);
+    
+        likeListModel = new DefaultListModel<>();
+        likeList = new JList<>(likeListModel);
+        JScrollPane likeListScrollPane = new JScrollPane(likeList);
+        likeListScrollPane.setPreferredSize(new Dimension(50, 50));
+        listPanel.add(likeListScrollPane);
+    
+        dislikeListModel = new DefaultListModel<>();
+        dislikeList = new JList<>(dislikeListModel);
+        JScrollPane dislikeListScrollPane = new JScrollPane(dislikeList);
+        dislikeListScrollPane.setPreferredSize(new Dimension(50, 50));
+        listPanel.add(dislikeListScrollPane);
+    
+        mainPanel.add(listPanel);
+        listPanel.setPreferredSize(new Dimension(50, 20));
+    
+        submitButton = new JButton("Submit");
+        mainPanel.add(submitButton);
+    
+        likeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String food = searchField.getText().trim();
+                if (!food.isEmpty() && !likeListModel.contains(food)) {
+                    likeListModel.addElement(food);
+                }
+            }
+        });
+    
+        dislikeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String food = searchField.getText().trim();
+                if (!food.isEmpty() && !dislikeListModel.contains(food)) {
+                    dislikeListModel.addElement(food);
+                }
+            }
+        });
+    
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Process and submit the information here
+                // For example, store them in a database or send them to an API
+            }
+        });
+    }
+    
+}
