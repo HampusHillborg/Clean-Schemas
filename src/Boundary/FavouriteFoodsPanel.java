c0ac7a88361ba5e21129f8c73ed6c572ffe56874
muhammedosman01 <128679878+muhammedosman01@users.noreply.github.com>
2023-04-18 09:05:32 +0200

    Gjort en JFrame med två paneler, en för mat man giller och en för mat man inte gillar

--- src/Boundary/FavouriteFoodsPanel.java
+++ src/Boundary/FavouriteFoodsPanel.java
@@ -2,8 +2,7 @@ package src.Boundary;
 
 import src.Entity.Profile;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.*;
 import javax.swing.*;
 
 public class FavouriteFoodsPanel extends JFrame {
@@ -14,7 +13,7 @@ public class FavouriteFoodsPanel extends JFrame {
     private JCheckBox fishCheckBox;
 
     public FavouriteFoodsPanel(Profile userProfile) {
-        // Set the layout manager for the panel
+         //Set the layout manager for the panel
         setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
@@ -24,10 +23,45 @@ public class FavouriteFoodsPanel extends JFrame {
         constraints.weighty = 1.0;
         constraints.insets.bottom = 10;
 
+        JFrame frame = new JFrame("Food Chooser");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+         //Create two panels for food choices
+        JPanel likePanel = new JPanel();
+        likePanel.setBorder(BorderFactory.createTitledBorder("Food I Like"));
+        JList<String> likeList = new JList<>(new String[]{"Rice", "Pasta", "Chicken", "Beef","Fish"});
+        likeList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        JScrollPane likeScrollPane = new JScrollPane(likeList);
+        likePanel.add(likeScrollPane);
+
+        JPanel dislikePanel = new JPanel();
+        dislikePanel.setBorder(BorderFactory.createTitledBorder("Food I Don't Like"));
+        JList<String> dislikeList = new JList<>(new String[]{"Rice", "Pasta", "Chicken", "Beef","Fish"});
+        dislikeList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        JScrollPane dislikeScrollPane = new JScrollPane(dislikeList);
+        dislikePanel.add(dislikeScrollPane);
+
+        // Create the split pane and add the panels to it
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, likePanel, dislikePanel);
+        splitPane.setDividerLocation(0.5);
+        splitPane.setResizeWeight(0.5);
+        splitPane.setOneTouchExpandable(true);
+
+                // Add the split pane to the frame
+        frame.getContentPane().add(splitPane);
+
+        // Display the frame
+        frame.setVisible(true);
+
         // Add a title label to the panel
         JLabel titleLabel = new JLabel("Choose your preferred carbohydrates and proteins:");
         titleLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         add(titleLabel, constraints);
+
+        JPanel lp= new JPanel( new GridLayout(1,2));
+        lp.setBorder(BorderFactory.createTitledBorder("Food I Like"));
+
 
         // Add a rice checkbox to the panel
         riceCheckBox = new JCheckBox("Rice");
@@ -55,7 +89,7 @@ public class FavouriteFoodsPanel extends JFrame {
         add(fishCheckBox, constraints);
 
         // Set size and make visible
-        setSize(400, 300);
+        setSize(500, 500);
         setLocationRelativeTo(null);
         setVisible(true);
     }
