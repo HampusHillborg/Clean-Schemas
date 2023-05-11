195645b1d35b4030ca270cd3fb4c6c42b21e454f
Imaznaseem <imaznaseem@gmail.com>
2023-05-11 11:53:39 +0200

    JavaDoc comments in LoginViewerGUI-class on methods that were missing them.

--- src/Boundary/LoginViewerGUI.java
+++ src/Boundary/LoginViewerGUI.java
@@ -12,6 +12,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
 
+
 /**
  * The LoginViewerGUI class represents the graphical user interface for the
  * login screen of the application.
@@ -40,20 +41,27 @@ public class LoginViewerGUI extends JFrame implements ActionListener, KeyListene
         createUI();
     }
 
+    /**
+     * Creates the user interface for the LoginViewerGUI and sets its layout, components, and settings.
+     * The user interface includes a form panel containing email and password fields, and a panel
+     * containing the create and login buttons.
+     */
     private void createUI() {
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-    
+
         JLabel usernameLabel = new JLabel("Email:");
         usernameField = new JTextField(20);
-    
+
         JLabel passwordLabel = new JLabel("Password:");
         passwordField = new JPasswordField(20);
         passwordField.addKeyListener(this);
-    
+
         createButton = createButton("Create Account");
         loginButton = createButton("Log In");
-    
+        createButton.setOpaque(true);
+        loginButton.setOpaque(true);
+
         JPanel formPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
@@ -61,30 +69,36 @@ public class LoginViewerGUI extends JFrame implements ActionListener, KeyListene
         gbc.anchor = GridBagConstraints.WEST;
         gbc.insets = new Insets(5, 5, 5, 5);
         formPanel.add(usernameLabel, gbc);
-    
+
         gbc.gridx = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         formPanel.add(usernameField, gbc);
-    
+
         gbc.gridx = 0;
         gbc.gridy = 1;
         formPanel.add(passwordLabel, gbc);
-    
+
         gbc.gridx = 1;
         formPanel.add(passwordField, gbc);
-    
+
         mainPanel.add(formPanel);
         mainPanel.add(createButtonPanel());
-    
+
         add(mainPanel);
-    
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
     }
-    
 
+    /**
+     * Creates a JButton with the specified text and sets its alignment, background color,
+     * foreground color, action listener, and opacity.
+     *
+     * @param text the text to display on the button
+     * @return a JButton with the specified text and settings
+     */
     private JButton createButton(String text) {
         JButton button = new JButton(text);
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -98,22 +112,26 @@ public class LoginViewerGUI extends JFrame implements ActionListener, KeyListene
     private JPanel createFormField(JLabel label, JTextField textField) {
         JPanel panel = new JPanel(new GridBagLayout());
         panel.setAlignmentX(Component.CENTER_ALIGNMENT);
-    
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.WEST;
         gbc.insets = new Insets(5, 5, 5, 5);
         panel.add(label, gbc);
-    
+
         gbc.gridx = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(textField, gbc);
-    
+
         return panel;
     }
-    
 
+    /**
+     * Creates a JPanel containing the create and login buttons.
+     *
+     * @return a JPanel containing the create and login buttons
+     */
     private JPanel createButtonPanel() {
         JPanel panel = new JPanel();
         panel.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -123,6 +141,13 @@ public class LoginViewerGUI extends JFrame implements ActionListener, KeyListene
         return panel;
     }
 
+    /**
+     * Called when an action is performed on the create or login button.
+     * If the source of the action event is the create button, the createAccount() method is called.
+     * If the source of the action event is the login button, the login() method is called.
+     *
+     * @param e the event to be processed
+     */
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == createButton) {
             createAccount();
@@ -216,10 +241,22 @@ public class LoginViewerGUI extends JFrame implements ActionListener, KeyListene
         }
     }
 
+    /**
+     * Invoked when a key has been released.
+     *
+     * @param e the event to be processed
+     */
     public void keyReleased(KeyEvent e) {
         // do nothing
     }
 
+    /**
+     * The main method for the LoginViewerGUI class.
+     * This method creates a UserDatabase object and a new instance of the LoginViewerGUI class,
+     * which is executed on the event dispatching thread using SwingUtilities.invokeLater().
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             UserDatabase userDatabase = new UserDatabase();
