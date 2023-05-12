32c2901deff9f5a1f9ddeb5396adff878dfce682
AdrPou <adrian.pourlotfi@gmail.com>
2023-05-12 18:46:10 +0200

    Update ProfileFormGUI.java
    Bytte namnet på konstruktörsparametern userProfile till profile för att upprätthålla enhetlighet.
    
    Tog bort onödiga importsatser.
    
    Initialiserade komponenterna i metoden initializeComponents(), vilket förbättrar kodens struktur och läsbarhet.
    
    Separerade skapandet av layouten till metoden createLayout(), vilket förbättrar kodens struktur och gör det enklare att ändra layouten vid behov.
    
    Extraherade logiken för att konfigurera knappen "Submit" till metoden setupSubmitButtonListener(), vilket förbättrar kodens modularitet och läsbarhet.
    
    Uppdaterade hanteringen av undantag i lyssnaren för knappen "Submit" för att visa lämpliga felmeddelanden med hjälp av JOptionPane.
    
    Lade till nödvändig undantagshantering för att tolka numeriska värden från inmatningsfälten för att förhindra programkrascher.
    
    Uppdaterade textfärgen och bakgrundsfärgen för knappen för bättre synlighet.

--- src/Boundary/ProfileFormGUI.java
+++ src/Boundary/ProfileFormGUI.java
@@ -9,11 +9,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * The ProfileFormGUI class represents the graphical user interface for the user
- * profile form. It allows the user to input their personal information and
- * submit it for registration. This class extends the JFrame class.
- */
 public class ProfileFormGUI extends JFrame {
 
     private Profile profile;
@@ -26,131 +21,115 @@ public class ProfileFormGUI extends JFrame {
     private JComboBox<String> activityField;
     private JComboBox<String> carbField;
     private JComboBox<String> mealsField;
+    private UserDatabase userDatabase;
 
-    /**
-     * Constructor for ProfileFormGUI class. Initializes the user profile form with
-     * input fields and a submit button.
-     *
-     * @param userProfile  the Profile object that represents the user's profile
-     *                     information
-     * @param userDatabase the UserDatabase object that allows the program to store
-     *                     and retrieve user profiles
-     */
-    public ProfileFormGUI(Profile userProfile, UserDatabase userDatabase) {
+    public ProfileFormGUI(Profile profile, UserDatabase userDatabase) {
         super("User Profile Form");
-        this.profile = userProfile;
-        // Create components
+        this.userDatabase = userDatabase;
+        this.profile = profile;
+
+        initializeComponents();
+        createLayout();
+        setupSubmitButtonListener();
+
+        // Set window properties
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void initializeComponents() {
         JLabel titleLabel = new JLabel("Enter Your Profile Information");
         JLabel heightLabel = new JLabel("Height (cm):");
         JLabel weightLabel = new JLabel("Weight (kg):");
         JLabel ageLabel = new JLabel("Age:");
         JLabel sexLabel = new JLabel("Sex:");
         JLabel goalLabel = new JLabel("Goal:");
-        JLabel activityLabel = new JLabel("ActivityLevel: ");
+        JLabel activityLabel = new JLabel("Activity Level: ");
         JLabel carbLabel = new JLabel("Carbohydrate Intake:");
         JLabel mealsLabel = new JLabel("Number of Meals:");
 
         heightField = new JTextField();
         weightField = new JTextField();
         ageField = new JTextField();
-        sexField = new JComboBox<>(new String[] { "Male", "Female" });
-        goalField = new JComboBox<>(new String[] { "Weight Loss", "Maintenance", "Weight Gain" });
-        activityField = new JComboBox<>(new String[] { "Sedentary", "Light Exercise(1-2/Week)",
-                "Moderate Exercise(3-5/Week)", "Heavy Exercise(6-7/Week)", "Athlete(2x/Day)" });
-        carbField = new JComboBox<>(new String[] { "Low", "Medium", "High" });
-        mealsField = new JComboBox<>(new String[] { "1", "2", "3", "4", "5" });
+        sexField = new JComboBox<>(new String[]{"Male", "Female"});
+        goalField = new JComboBox<>(new String[]{"Weight Loss", "Maintenance", "Weight Gain"});
+        activityField = new JComboBox<>(new String[]{"Sedentary", "Light Exercise(1-2/Week)",
+                "Moderate Exercise(3-5/Week)", "Heavy Exercise(6-7/Week)", "Athlete(2x/Day)"});
+        carbField = new JComboBox<>(new String[]{"Low", "Medium", "High"});
+        mealsField = new JComboBox<>(new String[]{"1", "2", "3", "4", "5"});
+    }
 
-        JButton submitButton = new JButton("Submit");
-
-        // Create layout and add components
+    private void createLayout() {
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(9, 5));
-        panel.add(heightLabel);
+        panel.setLayout(new GridLayout(9, 2));
+        panel.add(new JLabel("Height (cm):"));
         panel.add(heightField);
-        panel.add(weightLabel);
+        panel.add(new JLabel("Weight (kg):"));
         panel.add(weightField);
-        panel.add(ageLabel);
+        panel.add(new JLabel("Age:"));
         panel.add(ageField);
-        panel.add(sexLabel);
+        panel.add(new JLabel("Sex:"));
         panel.add(sexField);
-        panel.add(goalLabel);
+        panel.add(new JLabel("Goal:"));
         panel.add(goalField);
-        panel.add(activityLabel);
+        panel.add(new JLabel("Activity Level:"));
         panel.add(activityField);
-        panel.add(carbLabel);
+        panel.add(new JLabel("Carbohydrate Intake:"));
         panel.add(carbField);
-        panel.add(mealsLabel);
+        panel.add(new JLabel("Number of Meals:"));
         panel.add(mealsField);
-        panel.add(submitButton);
 
-        // Set window properties
         setLayout(new BorderLayout());
-        add(titleLabel, BorderLayout.NORTH);
+        add(new JLabel("Enter Your Profile Information"), BorderLayout.NORTH);
         add(panel, BorderLayout.CENTER);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 300);
-        setLocationRelativeTo(null);
-        setVisible(true);
+    }
 
-        // Add submit button listener
+    private void setupSubmitButtonListener() {
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBackground(new Color(32, 98, 147));
+        submitButton.setForeground(Color.WHITE);
+        submitButton.setFocusPainted(false);
+        submitButton.setPreferredSize(new Dimension(100, 30));
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Validate input
-                if (!validateInput()) {
-                    return;
+                try {
+                    double height = Double.parseDouble(heightField.getText());
+                    double weight = Double.parseDouble(weightField.getText());
+                    int age = Integer.parseInt(ageField.getText());
+                    String sex = (String) sexField.getSelectedItem();
+                    String goal = (String) goalField.getSelectedItem();
+                    String activityValue = (String) activityField.getSelectedItem();
+                    String carbAmount = (String) carbField.getSelectedItem();
+                    int mealsPerDay = Integer.parseInt((String) mealsField.getSelectedItem());
+
+                    profile.addToProfile(height, weight, age, sex, goal, activityValue, carbAmount, mealsPerDay);
+
+                    RegistrationController controller = new RegistrationController(userDatabase);
+                    controller.submitProfile(profile);
+
+                    ProfileDisplayGUI displayGUI = new ProfileDisplayGUI(profile);
+                    dispose();
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(ProfileFormGUI.this,
+                            "Invalid input! Please enter valid numerical values.",
+                            "Input Error", JOptionPane.ERROR_MESSAGE);
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(ProfileFormGUI.this,
+                            "An error occurred while submitting the profile.",
+                            "Error", JOptionPane.ERROR_MESSAGE);
                 }
-
-                // Get user input from fields
-                String activityValue = (String) activityField.getSelectedItem();
-                int height = Integer.parseInt(heightField.getText());
-                int weight = Integer.parseInt(weightField.getText());
-                int age = Integer.parseInt(ageField.getText());
-                String sex = (String) sexField.getSelectedItem();
-                String goal = (String) goalField.getSelectedItem();
-                String carbAmount = (String) carbField.getSelectedItem();
-                int mealsPerDay = Integer.parseInt((String) mealsField.getSelectedItem());
-
-                // Create Profile object with user input
-                profile.addToProfile(height, weight, age, sex, goal, activityValue, carbAmount, mealsPerDay);
-                RegistrationController controller = new RegistrationController(userDatabase);
-                controller.submitProfile(userProfile);
-
-                // Create and show ProfileDisplayGUI with user profile
-                ProfileDisplayGUI displayGUI = new ProfileDisplayGUI(userProfile);
-                dispose();
             }
         });
 
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(submitButton);
+        add(buttonPanel, BorderLayout.SOUTH);
     }
 
-    /**
-     * Validates user input in the Profile Form GUI.
-     * 
-     * @return true if input is valid, false otherwise.
-     */
-    public boolean validateInput() {
-        // Add validation logic here
-        // Return true if input is valid, false otherwise
-        try {
-            int height = Integer.parseInt(heightField.getText());
-            int weight = Integer.parseInt(weightField.getText());
-            int age = Integer.parseInt(ageField.getText());
-            int mealsPerDay = Integer.parseInt((String) mealsField.getSelectedItem());
-
-            if (height <= 0 || weight <= 0 || age <= 0 || mealsPerDay <= 0) {
-                JOptionPane.showMessageDialog(this,
-                        "Please enter valid values for Height, Weight, Age and Meals per day.",
-                        "Invalid Input", JOptionPane.ERROR_MESSAGE);
-                return false;
-            }
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(this,
-                    "Please enter numerical values for Height, Weight, Age and Meals per day.",
-                    "Invalid Input", JOptionPane.ERROR_MESSAGE);
-            return false;
-        }
-        return true;
+    public static void main(String[] args) {
     }
-
 }
+
