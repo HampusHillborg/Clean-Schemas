eb533dc90414aa6aee1b48b6b3a6bfb924841a3f
Imaznaseem <imaznaseem@gmail.com>
2023-05-18 17:25:51 +0200

    Extract the common code from LandingPage() into methods: There were some repetitive code for creating buttons and labels. I extracted this code into separate methods to reduce duplication and improve readability.
    The new methods that were created: setupUI(), setupButtonsPanel(), addButtonWithLabel(), setupLogoutButton() and setupEventHandlers().

--- src/Boundary/LandingPage.java
+++ src/Boundary/LandingPage.java
@@ -24,10 +24,10 @@ public class LandingPage extends JFrame {
     private LoginController loginController;
 
     /**
-     * Constructs a LandingPage object with a given Profile and UserDatabase.
+     * Constructs a LandingPage object with the specified profile and user database.
      *
-     * @param profile the user's profile information
-     * @param userDatabase the database containing user information
+     * @param profile The user profile.
+     * @param userDatabase The user database.
      */
     public LandingPage(Profile profile, UserDatabase userDatabase) {
         super("Main Menu");
@@ -36,111 +36,118 @@ public class LandingPage extends JFrame {
         setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-
-        // Set background color to black
         getContentPane().setBackground(Color.WHITE.brighter());
+        setupUI();
+        setupEventHandlers(profile);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 
+    /**
+     * Sets up the user interface by calling the necessary setup methods.
+     * Configures the buttons panel and the logout button.
+     */
+    private void setupUI() {
+        setupButtonsPanel();
+        setupLogoutButton();
+    }
 
-        UIManager.put("Label.foreground", Color.BLACK);
-        UIManager.put("Button.foreground", Color.BLACK);
-        UIManager.put("Button.focus", Color.BLACK);
-        UIManager.put("Button.border", BorderFactory.createEmptyBorder());
-
-
+    /**
+     * Sets up the buttons panel in the user interface.
+     * Creates a panel with a grid layout and adds buttons with corresponding labels to it.
+     * The buttons represent different functionalities in the application.
+     */
+    private void setupButtonsPanel() {
         JPanel buttonsPanel = new JPanel();
         buttonsPanel.setLayout(new GridLayout(5, 2, 20, 20));
         buttonsPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         add(buttonsPanel, BorderLayout.CENTER);
 
-        // Create a logout button and add it to the top right corner
+        addButtonWithLabel(buttonsPanel, "Meals",
+                "<html><p style='width:150px;'>Heres where you see the meals for the day. You can either choose which foods you want to eat for a certain meal or you can let us generate a meal for you!</p></html>");
+        addButtonWithLabel(buttonsPanel, "Search for foods",
+                "<html><p style='width:150px;'>Here you can search for a food to see how much protein, carbs, fat and calories it contains per 100 grams</p></html>");
+        addButtonWithLabel(buttonsPanel, "Choose Foods",
+                "<html><p style='width:150px;'>Here you will have the options to add foods to a favourite list or dislike list so that we can generate better suited meals for you.</p></html>");
+        addButtonWithLabel(buttonsPanel, "Edit Profile",
+                "<html><p style='width:150px;'>Set up your personal info so that we can calculate your needs and make personalized mealplans.</p></html>");
+        addButtonWithLabel(buttonsPanel, "Profilestatus",
+                "<html><p style='width:150px;'>Show your personal info.</p></html>");
+
+        buttonsPanel.setBackground(Color.WHITE.brighter());
+    }
+
+    /**
+     * Adds a button with a corresponding label to the specified panel.
+     *
+     * @param panel The panel to which the button and label are added.
+     * @param buttonText The text displayed on the button.
+     * @param labelText  The text displayed on the label.
+     */
+    private void addButtonWithLabel(JPanel panel, String buttonText, String labelText) {
+        JButton button = createStyledButton(buttonText);
+        JLabel label = createStyledLabel(labelText);
+        label.setVerticalAlignment(JLabel.TOP);
+        label.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
+        panel.add(button);
+        panel.add(label);
+    }
+
+    /**
+     * Sets up the logout button in the user interface.
+     * The button is added to the top right corner of the layout.
+     * When clicked, it triggers the logout functionality.
+     */
+    private void setupLogoutButton() {
         JButton logoutButton = createStyledButton("Logout");
-        logoutButton.setPreferredSize(new Dimension(100, 40)); // Updated size
+        logoutButton.setPreferredSize(new Dimension(100, 40));
         JPanel logoutPanel = new JPanel();
         logoutPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        logoutPanel.add(logoutButton);
         logoutPanel.setBackground(Color.WHITE.brighter());
+        logoutPanel.add(logoutButton);
         add(logoutPanel, BorderLayout.NORTH);
+    }
 
-        JButton mealsButton = createStyledButton("Meals");
-        JLabel mealsLabel = createStyledLabel(
-                "<html><p style='width:150px;'>Heres where you see the meals for the day. You can either choose which foods you want to eat for a certain meal or you can let us generate a meal for you!</p></html>");
-        mealsLabel.setVerticalAlignment(JLabel.TOP);
-        mealsLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-        buttonsPanel.add(mealsButton);
-        buttonsPanel.add(mealsLabel);
-        buttonsPanel.setBackground(Color.WHITE.brighter());
+    /**
+     * Sets up the event handlers for the buttons in the user interface.
+     *
+     * @param profile The user profile used for handling the events.
+     */
+    private void setupEventHandlers(Profile profile) {
+        Component[] components = getContentPane().getComponents();
+        if (components.length >= 1 && components[0] instanceof JPanel) {
+            JPanel buttonsPanel = (JPanel) components[0];
+            for (Component component : buttonsPanel.getComponents()) {
+                if (component instanceof JButton) {
+                    JButton button = (JButton) component;
+                    if (button.getText().equals("Profilestatus")) {
+                        button.addActionListener(e -> new ProfileDisplayGUI(profile));
+                    } else if (button.getText().equals("Meals")) {
+                        button.addActionListener(e -> new MealsButton(profile, userDatabase));
+                    } else if (button.getText().equals("Search for foods")) {
+                        button.addActionListener(e -> new SearchForFoods());
+                    } else if (button.getText().equals("Choose Foods")) {
+                        button.addActionListener(e -> new DietPreferencesView(profile, userDatabase));
+                    } else if (button.getText().equals("Edit Profile")) {
+                        button.addActionListener(e -> new DisplayProfile(profile, userDatabase));
+                    }
+                }
+            }
+        }
 
-        JButton searchForFoods = createStyledButton("Search for foods");
-        JLabel weeklyPlansLabel = createStyledLabel(
-                "<html><p style='width:150px;'>Here you can search for a food to see how much protein, carbs, fat and calories it contains per 100 grams</p></html>");
-        weeklyPlansLabel.setVerticalAlignment(JLabel.TOP);
-        weeklyPlansLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-        buttonsPanel.add(searchForFoods);
-        buttonsPanel.add(weeklyPlansLabel);
-        buttonsPanel.setBackground(Color.WHITE.brighter());
-
-        JButton chooseFoodsButton = createStyledButton("Choose Foods");
-        JLabel chooseFoodsLabel = createStyledLabel(
-                "<html><p style='width:150px;'>Here you will have the options to add foods to a favourite list or dislike list so that we can generate better suited meals for you.</p></html>");
-        chooseFoodsLabel.setVerticalAlignment(JLabel.TOP);
-        chooseFoodsLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-        buttonsPanel.add(chooseFoodsButton);
-        buttonsPanel.add(chooseFoodsLabel);
-        buttonsPanel.setBackground(Color.WHITE.brighter());
-
-        JButton profileButton = createStyledButton("Edit Profile");
-        JLabel profileLabel = createStyledLabel(
-                "<html><p style='width:150px;'>Set up your personal info so that we can calculate your needs and make personalized mealplans.</p></html>");
-        profileLabel.setVerticalAlignment(JLabel.TOP);
-        profileLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-        buttonsPanel.add(profileButton);
-        buttonsPanel.add(profileLabel);
-        buttonsPanel.setBackground(Color.WHITE.brighter());
-
-        JButton savedInfoButton = createStyledButton("Profilestatus");
-        JLabel savedInfoLabel = createStyledLabel("<html><p style='width:150px;'>Show your personal info.</p></html>");
-        savedInfoLabel.setVerticalAlignment(JLabel.TOP);
-        savedInfoLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-        buttonsPanel.add(savedInfoButton);
-        buttonsPanel.add(savedInfoLabel);
-        buttonsPanel.setBackground(Color.WHITE.brighter());
-
-        add(buttonsPanel, BorderLayout.WEST);
-
-        savedInfoButton.addActionListener(e -> {
-            new ProfileDisplayGUI(profile);
-        });
-
-        mealsButton.addActionListener(e -> {
-            // Code to execute when mealsButton is clicked
-            new MealsButton(profile, userDatabase);
-
-        });
-
-        searchForFoods.addActionListener(e -> {
-            new SearchForFoods();
-        });
-
-        chooseFoodsButton.addActionListener(e -> {
-            // Code to execute when mealsButton is clicked
-            new DietPreferencesView(profile, userDatabase);
-
-        });
-
-        profileButton.addActionListener(e -> {
-            new DisplayProfile(profile, userDatabase);
-            // Code to execute when mealsButton is clicked
-
-
-        });
-
-        logoutButton.addActionListener(e -> {
-            new LoginViewerGUI(userDatabase);
-            dispose();
-        });
-
-        setLocationRelativeTo(null);
-        setVisible(true);
+        Component[] logoutComponents = getContentPane().getComponents();
+        if (logoutComponents.length >= 2 && logoutComponents[1] instanceof JPanel) {
+            JPanel logoutPanel = (JPanel) logoutComponents[1];
+            for (Component component : logoutPanel.getComponents()) {
+                if (component instanceof JButton && ((JButton) component).getText().equals("Logout")) {
+                    JButton logoutButton = (JButton) component;
+                    logoutButton.addActionListener(e -> {
+                        new LoginViewerGUI(userDatabase);
+                        dispose();
+                    });
+                }
+            }
+        }
     }
 
     /**
